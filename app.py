6e570d3f9054e86331428561728909c52c5831b9
graceamulya <graceamulya05@gmail.com>
2025-08-02 12:25:45 +0530

    second update

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app=Flask(__name__)
 #create templates folder => index.html
 def index():
     return "Welcome to our College"
-@app.route("/home")
-def home():
+@app.route("/home1")
+def home1():
     return "home page"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)

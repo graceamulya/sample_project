8964ba345518fcb0a1140e6556b4bdc80d4a8975
graceamulya <grcaeamulya05@gmail.com>
2025-08-02 11:50:15 +0530

    firstupdate

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app=Flask(_name_)
+app=Flask(__name__)
 @app.route("/")
 def index():
     return "Welcome to our College"
-if _name=="main_":
+@app.route("/home")
+def home():
+    return "home page"
+if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
